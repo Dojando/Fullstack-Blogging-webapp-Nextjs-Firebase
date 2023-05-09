fd45a7c4d865721d8577b72306ee84828e715171
jawad <wadja.b@hotmail.fr>
2023-05-09 23:48:33 +0200

    gestion de la page d'utilisateur via server side rendering

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,35 @@
-export default function UserProfilPage({  }) {
+import { getUserWithUsername, postToJSON } from '../../lib/firebase';
+import UserProfile from '../../components/UserProfile';
+import PostFeed from '../../components/PostFeed';
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
     <main>
-      <p>Profil d'utilisateur</p>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
-  )
+  );
 }
