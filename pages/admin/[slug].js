a6b7f96c45c48518da9e4e1df538d838b9515b85
jawad <wadja.b@hotmail.fr>
2023-05-10 18:32:32 +0200

    création d'un composant Metatags avec des tags modulaires pour améliorer le référencement

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,6 +1,9 @@
+import Metatags from '../../components/Metatags';
+
 export default function AdminPostEdit({  }) {
   return (
     <main>
+      <Metatags title="page admin" />
     </main>
   )
 }
